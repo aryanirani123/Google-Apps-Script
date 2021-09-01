066544f32bc3bdf6d86c415cb52279aa9c0ac57a
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2021-09-01 09:30:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+//This code is written in Google Apps Script(JavaScript). 
+//This code changes the colours of cells in Google Sheets on meeting the given criteria. 
+//In this example, if the number in the cell is greater than 3, the cell colour to green. 
+
 function ChangeColor(){
 
-  
 var sheet = SpreadsheetApp.getActiveSheet();
 var range = sheet.getRange("D1:D10");
 var rule = SpreadsheetApp.newConditionalFormatRule()
@@ -12,7 +15,6 @@ var rules = sheet.getConditionalFormatRules();
 rules.push(rule);
 sheet.setConditionalFormatRules(rules);
 
-
 var rule = SpreadsheetApp.newConditionalFormatRule()
     .whenNumberGreaterThan(3)
     .setBackground("green")
@@ -20,9 +22,6 @@ var rule = SpreadsheetApp.newConditionalFormatRule()
     .build();
 var rules = sheet.getConditionalFormatRules();
 rules.push(rule);
-sheet.setConditionalFormatRules(rules); 
-  
-  
-  
+sheet.setConditionalFormatRules(rules);   
   
 }
