2c22e1b120acd7fea41354096d3ece56266d4032
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2021-11-16 10:45:28 +0530

    Create CustomFunction.js

---
+++ CustomFunction.js
@@ -0,0 +1,14 @@
+// This code is wriiten in Google Apps Script(JavaScript)
+// This code adds timestamp to Google Sheets using custom functions. 
+// To add a Timestamp to a cell, use the name of the function and pass the parameter.
+
+
+function SetTimestamp(x) {
+
+  if(x != ""){
+
+    return new Date();
+
+  }
+
+}
